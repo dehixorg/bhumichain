27deaa2fb353be21e74bbab63f09eeee60c32a51
coderaryanlpu <aryanmaheshwari6631@gmail.com>
2026-07-04 17:44:50 +0530

    fix: docker exec syntax in upgrade script

--- infra/fabric-network/scripts/upgrade-property-transfer.sh
+++ infra/fabric-network/scripts/upgrade-property-transfer.sh
@@ -33,10 +33,10 @@ docker exec fabric-network-cli-1 peer lifecycle chaincode install "/tmp/${NAME}.
 
 if [ "$PEER1_RUNNING" = "true" ]; then
   echo "  [2/5] Installing on peer1..."
-  docker exec fabric-network-cli-1 \
+  docker exec \
     -e CORE_PEER_ADDRESS=peer1.revenuedept.bhumichain.in:9051 \
     -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/revenuedept.bhumichain.in/peers/peer1.revenuedept.bhumichain.in/tls/ca.crt \
-    cli peer lifecycle chaincode install "/tmp/${NAME}.tar.gz"
+    fabric-network-cli-1 peer lifecycle chaincode install "/tmp/${NAME}.tar.gz"
 fi
 
 echo "  [3/5] Getting package ID..."
